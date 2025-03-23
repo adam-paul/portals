02c883251e2462b0a7af58eb4df4574bf7c15331
Adam Paul <notadampaul@protonmail.com>
2025-03-23 16:44:11 -0500

    fix escape characters

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -135,14 +135,14 @@ const Index: React.FC = () => {
                 >
                   <Link to="/games">
                     <button className="text-2xl font-bold tracking-wider text-space-light hover:text-space-light/80 transition-all duration-300">
-                      > ALL GAMES
+                      &gt; ALL GAMES
                     </button>
                   </Link>
                   <button 
                     onClick={handleRandomGame}
                     className="text-2xl font-bold tracking-wider text-space-light hover:text-space-light/80 transition-all duration-300"
                   >
-                    > I'M FEELING LUCKY
+                    &gt; I'M FEELING LUCKY
                   </button>
                 </motion.div>
               )}
